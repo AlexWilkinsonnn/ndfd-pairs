d00c4409b39e07a76a0d89ca241c1e008ac65b71
Colin Weber <colweber@dunegpvm14.fnal.gov>
2024-12-12 11:20:41 -0600

    Grabbing new commit to nd-sim-tools that makes ND PRISM cuts at event generation stage.

--- submit_n_grid_jobs.sh
+++ submit_n_grid_jobs.sh
@@ -6,6 +6,7 @@
 #	[-i input script] \
 #	[-t input tarbell] \
 #	[-n number of times to run jobsub_submit.py]
+# Ex: bash submit_n_grid_jobs.sh -i /exp/dune/app/users/colweber/ndfd-pairs/ndfd_depos/nd-sim-tools/produce_scripts/produce_edep-paramreco_larbath_transrots_tdr.sh -t /exp/dune/app/users/colweber/ndfd-pairs/ndfd_depos/nd-sim-tools/inputs/jobdata.tar.gz -n 150
 
 # She-bang!
 #!/bin/bash
@@ -21,8 +22,8 @@ submissions_per_job=200
 # Execute the for loop
 for (( i=1; i<=$n_submits; i++ ));
 do
-	offset=$(($submissions_per_job * ($i - 1)))
+	offset=$(($submissions_per_job * ($i - 1) + 7400))
 
-	jobsub_submit -G dune -N $submissions_per_job --disk=10Gb --memory=3000MB --expected-lifetime=10h --cpu=1 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC,OFFSITE --tar_file_name=dropbox://$input_tar --use-cvmfs-dropbox -l '+SingularityImage=\"/cvmfs/singularity.opensciencegrid.org/fermilab/fnal-wn-sl7:latest\"' --append_condor_requirements='(TARGET.HAS_Singularity==true&&TARGET.HAS_CVMFS_dune_opensciencegrid_org==true&&TARGET.HAS_CVMFS_larsoft_opensciencegrid_org==true&&TARGET.CVMFS_dune_opensciencegrid_org_REVISION>=1105&&TARGET.HAS_CVMFS_fifeuser1_opensciencegrid_org==true&&TARGET.HAS_CVMFS_fifeuser2_opensciencegrid_org==true&&TARGET.HAS_CVMFS_fifeuser3_opensciencegrid_org==true&&TARGET.HAS_CVMFS_fifeuser4_opensciencegrid_org==true)' file://$input_script $offset 1e15
+	jobsub_submit -G dune -N $submissions_per_job --disk=120Gb --memory=3000MB --expected-lifetime=6h --cpu=1 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC,OFFSITE --tar_file_name=dropbox://$input_tar --use-cvmfs-dropbox -l '+SingularityImage=\"/cvmfs/singularity.opensciencegrid.org/fermilab/fnal-wn-sl7:latest\"' --append_condor_requirements='(TARGET.HAS_Singularity==true&&TARGET.HAS_CVMFS_dune_opensciencegrid_org==true&&TARGET.HAS_CVMFS_larsoft_opensciencegrid_org==true&&TARGET.CVMFS_dune_opensciencegrid_org_REVISION>=1105&&TARGET.HAS_CVMFS_fifeuser1_opensciencegrid_org==true&&TARGET.HAS_CVMFS_fifeuser2_opensciencegrid_org==true&&TARGET.HAS_CVMFS_fifeuser3_opensciencegrid_org==true&&TARGET.HAS_CVMFS_fifeuser4_opensciencegrid_org==true&&TARGET.HAS_CVMFS_dune_osgstorage_org==true)' file://$input_script $offset 1e15
 	
 done
