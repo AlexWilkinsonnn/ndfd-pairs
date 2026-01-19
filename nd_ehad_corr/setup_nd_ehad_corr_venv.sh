db064e91b570a7803e9da7f191ff95190e52ce41
Colin Weber <colweber@dunegpvm02.fnal.gov>
2026-01-19 12:54:04 -0600

    Removed nd_ehad_corr module.

--- nd_ehad_corr/setup_nd_ehad_corr_venv.sh
+++
@@ -1,9 +0,0 @@
-venv_dir_name=".venv_3_9_2_nd_ehad_corr"
-
-source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
-setup python v3_9_2
-setup ifdhc
-mkdir -p ${venv_dir_name}
-python -m venv ${venv_dir_name}
-source ${venv_dir_name}/bin/activate
-pip install argparse h5py polars numpy matplotlib lmfit pandas scikit-learn
